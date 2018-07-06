4e4098a43f9b0ecc5f76bcb09af6ed9ae48b5c30
qilin <qilin@btb.com>
2018-07-06 11:43:54 +0800

    去掉大隐函数C

---
+++ MobileFrame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobileFrame
+//
+//  Created by dingqiankun on 2018/7/6.
+//  Copyright © 2018年 dingqiankun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
